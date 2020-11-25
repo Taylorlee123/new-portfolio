c74ceec3cfab6e17439f824cb462222ab290a937
Taylorlee123 <salzmantaylor@gmail.com>
2020-11-25 11:29:40 -0500

    profile image fix

--- src/mock/data.js
+++ src/mock/data.js
@@ -17,7 +17,7 @@ export const heroData = {
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile1.jpg',
+  img: 'profile1.png',
   paragraphOne: '',
   paragraphTwo: '',
   paragraphThree: '',
